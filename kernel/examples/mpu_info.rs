0e0f91f6f6cebb48c3baf38b1e1fe16ce35cc4b0
Kaido Kert <kaidokert@gmail.com>
2026-02-01 07:47:04 +0000

    Add MPU info example that reads TYPE register
    Read the MPU TYPE register via the safe cortex-m Peripherals API
    and print the DREGION count. QEMU lm3s6965evb reports 8 data regions.

---
+++ kernel/examples/mpu_info.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use cortex_m_semihosting::{debug, hprintln};
+use panic_semihosting as _;
+
+#[entry]
+fn main() -> ! {
+    let p = cortex_m::Peripherals::take().unwrap();
+    let mpu_type = p.MPU._type.read();
+    let dregion_count = (mpu_type >> 8) & 0xFF;
+    hprintln!("MPU regions: {}", dregion_count);
+    debug::exit(debug::EXIT_SUCCESS);
+    loop {
+        cortex_m::asm::wfi();
+    }
+}
