6ac18ce0a2356afea68a5e4ecfbc1dbfa1780fd8
Kaido Kert <kaidokert@gmail.com>
2026-03-15 14:07:41 +0000

    Remove duplicate kernel_new_is_canonical_constructor test
    The test duplicated the assertion already covered by
    kernel_new_delegates_to_new_external in the same module.
    Removing it to reduce redundancy.

--- kernel/src/svc/mod.rs
+++ kernel/src/svc/mod.rs
@@ -2951,31 +2951,6 @@ mod tests {
         assert_eq!(k.partitions().get(1).unwrap().entry_point(), 0x0800_1000);
     }
 
-    // ---- Kernel::new() is the canonical constructor ----
-
-    #[test]
-    fn kernel_new_is_canonical_constructor() {
-        let mut schedule = ScheduleTable::<4>::new();
-        schedule.add(ScheduleEntry::new(0, 5)).unwrap();
-        schedule.add(ScheduleEntry::new(1, 5)).unwrap();
-        schedule.start();
-
-        let mut stk0 = AlignedStack1K::default();
-        let mut stk1 = AlignedStack1K::default();
-        let mpu0 = MpuRegion::new(0x2000_0000, 4096, 0);
-        let mpu1 = MpuRegion::new(0x2000_1000, 4096, 0);
-        let m0 =
-            crate::partition::PartitionMemory::from_aligned_stack(&mut stk0, 0x0800_0000, mpu0, 0)
-                .unwrap();
-        let m1 =
-            crate::partition::PartitionMemory::from_aligned_stack(&mut stk1, 0x0800_1000, mpu1, 1)
-                .unwrap();
-        let k = Kernel::<TestConfig>::new(schedule, &[m0, m1]).unwrap();
-        assert_eq!(k.partitions().len(), 2);
-        assert_eq!(k.partitions().get(0).unwrap().entry_point(), 0x0800_0000);
-        assert_eq!(k.partitions().get(1).unwrap().entry_point(), 0x0800_1000);
-    }
-
     // ---- unpack_packed_r2 unit tests ----
 
     #[test]
